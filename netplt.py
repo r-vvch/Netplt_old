5036ef3244fad06ad37f9b6d1ef82a3984d38181
vovchenko.ra <vovchenko.ra@ispras.ru>
2022-05-04 19:20:18 +0300

    screw Windows support

--- netplt.py
+++ netplt.py
@@ -30,8 +30,8 @@ if __name__ == '__main__':
     if os.path.isdir(args.path):
         for file in os.listdir(args.path):
             if os.path.splitext(file)[1] == ".pcap":
-                subprocess.run(["python", "netplt.py", args.path + file, args.streams, str(time_unit)])
-            elif not os.path.isdir(args.path + "/" + file) or not os.path.isdir(args.path + "\\" + file):
+                subprocess.run(["python3", "netplt.py", args.path + file, args.mode, args.streams, str(time_unit)])
+            elif not os.path.isdir(args.path + "/" + file):
                 print(file + " is not .pcap file")
 
     elif os.path.isfile(args.path):
@@ -109,12 +109,7 @@ if __name__ == '__main__':
 
             now = datetime.now()
             now.replace(microsecond=0)
-            if "/" in args.path:
-                x = args.path.split("/")
-            elif "\\" in args.path:
-                x = args.path.split("\\")
-            else:
-                x = [args.path]
+            x = args.path.split("/")
             plt.savefig('streams_graph_grid_' + x[len(x) - 1].split(".")[0] + '.png')
 
         elif args.mode == "united":
@@ -146,16 +141,11 @@ if __name__ == '__main__':
                 plt.ylabel('bits/sec')
             else:
                 plt.ylabel('bits/' + str(time_unit) + 'sec')
-                
             now = datetime.now()
             now.replace(microsecond=0)
-            if "/" in args.path:
-                x = args.path.split("/")
-            elif "\\" in args.path:
-                x = args.path.split("\\")
-            else:
-                x = [args.path]
+            x = args.path.split("/")
             plt.title(x[len(x) - 1].split(".")[0])
+
             plt.savefig('streams_graph_united_' + x[len(x) - 1].split(".")[0] + '.png', bbox_extra_artists=(lgd,),
                         bbox_inches='tight')
 
