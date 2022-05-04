023278b9f0c92f7fc640e5f9f656b88ce16dff50
vovchenko.ra <vovchenko.ra@ispras.ru>
2022-05-04 19:08:04 +0300

    United plot added

--- README.md
+++ README.md
@@ -5,19 +5,19 @@ TCP streams graphing tool
 Required packages: tshark, matplotlib
 
 ```
-usage: netplt.py [-h] path [streams] [time_unit]
+usage: netplt.py [-h] path [mode] [streams] [time_unit]
 
 Build graphs for TCP steams
 
 positional arguments:
   path        Path to pcap file or directory with pcap files
+  mode        Graph type: grid or united plot
   streams     Streams to be plotted, space-separated
   time_unit   Time unit on the plot
 
 optional arguments:
   -h, --help  show this help message and exit
-
-usage example: netplt.py /home/roman/My/netplt_test/test.pcap "16 22" 10
 ```
- 
-[Example of work](https://helicopter.intra.ispras.ru/vovchenko.ra/netplt/-/blob/master/streams_graph_test.png)
+Work examples:  
+![grid](https://helicopter.intra.ispras.ru/vovchenko.ra/netplt/-/blob/master/streams_graph_grid_test.png)  
+![united](https://helicopter.intra.ispras.ru/vovchenko.ra/netplt/-/blob/master/streams_graph_united_test.png)
--- netplt.py
+++ netplt.py
@@ -20,6 +20,7 @@ class PacketInfo:
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build graphs for TCP steams')
     parser.add_argument('path', type=str, help='Path to pcap file or directory with pcap files')
+    parser.add_argument('mode', nargs='?', type=str, default='grid', help='Graph type: grid or united plot')
     parser.add_argument('streams', nargs='?', type=str, default='all', help='Streams to be plotted, space-separated')
     parser.add_argument('time_unit', nargs='?', type=int, default=1, help='Time unit on the plot')
     args = parser.parse_args()
@@ -29,7 +30,7 @@ if __name__ == '__main__':
     if os.path.isdir(args.path):
         for file in os.listdir(args.path):
             if os.path.splitext(file)[1] == ".pcap":
-                subprocess.run(["python3", "netplt.py", args.path + file, args.streams, str(time_unit)])
+                subprocess.run(["python3", "netplt.py", args.path + file, args.mode, args.streams, str(time_unit)])
             elif not os.path.isdir(args.path + "/" + file):
                 print(file + " is not .pcap file")
 
@@ -64,51 +65,92 @@ if __name__ == '__main__':
                     packet_storage[stream_num] = []
                     packet_storage[stream_num].append(PacketInfo(stream_num, packet.tcp.len, packet_time))
 
-        if len(packet_storage) == 1:
-            plt.rcParams["figure.figsize"] = (3, 3)
-        elif len(packet_storage) == 2:
-            plt.rcParams["figure.figsize"] = (6, 3)
-        elif selected_streams_str == 'all':
-            plt.rcParams["figure.figsize"] = (9, math.ceil((max_stream + 1) / 3) * 3)
-        else:
-            plt.rcParams["figure.figsize"] = (9, math.ceil(len(selected_streams) / 3) * 3)
+        if args.mode == "grid":
+            if len(packet_storage) == 1:
+                plt.rcParams["figure.figsize"] = (3, 3)
+            elif len(packet_storage) == 2:
+                plt.rcParams["figure.figsize"] = (6, 3)
+            elif selected_streams_str == 'all':
+                plt.rcParams["figure.figsize"] = (9, math.ceil((max_stream + 1) / 3) * 3)
+            else:
+                plt.rcParams["figure.figsize"] = (9, math.ceil(len(selected_streams) / 3) * 3)
 
-        pos = 1
-        for stream, stream_packets in packet_storage.items():
-            times = []
-            lengths = []
-            current_time = 0
-            while current_time < max_time:
-                interval_length = 0
-                for packet in stream_packets:
-                    if current_time < packet.time_relative < current_time + time_unit:
-                        interval_length += packet.length
-                times.append(current_time)
-                lengths.append(interval_length)
-                current_time += time_unit
-            if len(packet_storage) == 2:
-                plt.subplot(1, 2, pos)
-            elif len(packet_storage) > 2:
-                if selected_streams_str == 'all':
-                    plt.subplot(math.ceil((max_stream + 1) / 3), 3, stream + 1)
+            pos = 1
+            for stream, stream_packets in packet_storage.items():
+                times = []
+                lengths = []
+                current_time = 0
+                while current_time < max_time:
+                    interval_length = 0
+                    for packet in stream_packets:
+                        if current_time < packet.time_relative < current_time + time_unit:
+                            interval_length += packet.length
+                    times.append(current_time)
+                    lengths.append(interval_length)
+                    current_time += time_unit
+                if len(packet_storage) == 2:
+                    plt.subplot(1, 2, pos)
+                elif len(packet_storage) > 2:
+                    if selected_streams_str == 'all':
+                        plt.subplot(math.ceil((max_stream + 1) / 3), 3, stream + 1)
+                    else:
+                        plt.subplot(math.ceil(len(selected_streams) / 3), 3, pos)
+                times.append(max_time)
+                plt.stairs(lengths, times, fill=True)
+                plt.title(stream)
+                plt.xlabel('time')
+                if time_unit == 1:
+                    plt.ylabel('bits/sec')
                 else:
-                    plt.subplot(math.ceil(len(selected_streams) / 3), 3, pos)
-            times.append(max_time)
-            plt.stairs(lengths, times, fill=True)
-            plt.title(stream)
+                    plt.ylabel('bits/' + str(time_unit) + 'sec')
+                pos += 1
+
+            plt.tight_layout()
+
+            now = datetime.now()
+            now.replace(microsecond=0)
+            x = args.path.split("/")
+            plt.savefig('streams_graph_grid_' + x[len(x) - 1].split(".")[0] + '.png')
+
+        elif args.mode == "united":
+            plt.rcParams["figure.figsize"] = (6, 6)
+
+            pos = 1
+            for stream, stream_packets in packet_storage.items():
+                times = []
+                lengths = []
+                current_time = 0
+                while current_time < max_time:
+                    interval_length = 0
+                    for packet in stream_packets:
+                        if current_time < packet.time_relative < current_time + time_unit:
+                            interval_length += packet.length
+                    times.append(current_time)
+                    lengths.append(interval_length)
+                    current_time += time_unit
+                times.append(max_time)
+                plt.stairs(lengths, times, fill=True, label=stream)
+                pos += 1
+
+            if len(selected_streams) < 22:
+                lgd = plt.legend(bbox_to_anchor=(1, 1), loc='upper left')
+            else:
+                lgd = plt.legend(ncol=math.ceil((max_stream + 1) / 21), bbox_to_anchor=(1, 1), loc='upper left')
             plt.xlabel('time')
             if time_unit == 1:
                 plt.ylabel('bits/sec')
             else:
                 plt.ylabel('bits/' + str(time_unit) + 'sec')
-            pos += 1
+            now = datetime.now()
+            now.replace(microsecond=0)
+            x = args.path.split("/")
+            plt.title(x[len(x) - 1].split(".")[0])
 
-        plt.tight_layout()
+            plt.savefig('streams_graph_united_' + x[len(x) - 1].split(".")[0] + '.png', bbox_extra_artists=(lgd,),
+                        bbox_inches='tight')
 
-        now = datetime.now()
-        now.replace(microsecond=0)
-        x = args.path.split("/")
-        plt.savefig('streams_graph_' + x[len(x) - 1].split(".")[0] + '.png')
+        else:
+            print("Wrong input")
 
     else:
         print("Wrong input")
